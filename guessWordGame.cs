e4eeb9887f2d039db04b84bf33684170a607b114
SHIVAM SHANKHDHAR <39639414+shivamShankhdhar@users.noreply.github.com>
2020-06-08 09:25:05 +0530

    first commit

---
+++ guessWordGame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string guessWord = "shivam";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 4;
+            bool isOutOfGuesses = false;
+            
+            while(guess != guessWord && !isOutOfGuesses)
+            {
+                if(guessCount < guessLimit)
+                {
+                    Console.Write("Enter word: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                    if(guess != guessWord)
+                    {
+                        int guessesLeft = guessLimit - guessCount;
+                        Console.WriteLine("Guesses Left " + guessesLeft);
+                    }
+                   
+                }
+                else
+                {
+                    isOutOfGuesses = true;
+                }            
+            }
+            if( isOutOfGuesses)
+            {
+                Console.WriteLine("\nYou Lose!");
+            }
+            else
+            {
+                Console.WriteLine("\nYou Win! in " + guessCount + " " +"guesses");
+            }
+            
+        }
+        
+
+    }
+}
+
